9ff4ea101eb45cd9d082de409d75d6b27ea81efa
M.H.Udoy <mehedihasanudoybd@gmail.com>
2022-02-23 20:52:15 +0600

    update some data

--- lib/dice.dart
+++ lib/dice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Dice extends StatefulWidget {
   const Dice({Key? key}) : super(key: key);
 
@@ -9,11 +10,43 @@ class Dice extends StatefulWidget {
 class _DiceState extends State<Dice> {
   @override
   Widget build(BuildContext context) {
+    int ldrn=6;
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar:
         AppBar(
-          title: Text('Dice Changing'),
+          backgroundColor: Colors.green[500],
+          title: Text('Dicee Changing',
+            style:
+            TextStyle(
+              fontSize: 22,
+              color: Colors.white,
+            ),
+          ),
         ),
+      body: Center(
+        child: Row(
+          children: [
+
+            Expanded(child:
+            FlatButton(onPressed: () {
+              print('i am there');
+            },
+              child:Image.asset('images/dice$ldrn.png'),
+            ),
+            ),
+
+            Expanded(child:
+            FlatButton(onPressed: () {
+              print('i am here');
+            },
+            child:Image.asset('images/dice5.png'),
+            ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
